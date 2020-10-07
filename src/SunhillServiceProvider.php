92c1a7edc49c671be69210259aa5b9b67ab54c0c
klaus dimde <klaus@dimde.info>
2020-10-07 19:36:59 +0200

    Initial commit

---
+++ src/SunhillServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sunhill\ORM;
+
+use Illuminate\Support\ServiceProvider;
+use \Sunhill\ORM\Managers\class_manager;
+use \Sunhill\ORM\Managers\object_manager;
+use \Sunhill\ORM\Managers\tag_manager;
+
+class SunhillServiceProvider extends ServiceProvider
+{
+    public function register()
+    {        
+    }
+    
+    public function boot()
+    {
+    }
+}
