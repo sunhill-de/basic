f5174b1136bd6d5830faab1cad1d27b457e1c46b
klaus dimde <klaus@dimde.info>
2021-08-10 12:19:22 +0200

    Completed ScenarioWithFiles

--- src/Tests/Scenario/ScenarioWithFiles.php
+++ src/Tests/Scenario/ScenarioWithFiles.php
@@ -52,23 +52,44 @@ trait ScenarioWithFiles {
     
     protected function SetUpFiles() {
         $descriptors = $this->GetFiles();
-        foreach ($descriptors as $table => $descriptor) {
-            
+        foreach ($descriptors as $descriptor) {
+           $this->SetupFile($descriptor['path'],$descriptor['content']); 
         }
+    }
+    
+    protected function SetupFile(string $path,string $content) {
+        $path = $this->GetCompletePath($path);
+        $file = fopen($path,'w+');
+        fputs($file,$content);
+        fclose($file);
     }
     
     protected function SetUpDirs() {
         $descriptors = $this->GetDirs();
         foreach ($descriptors as $dir) {
-            
+            $this->SetupDir($dir);
         }        
+    }
+    
+    protected function SetupDir(string $dir) {
+        $dir = $this->GetCompletePath($dir);
+        exec('mkdir '.$dir);
     }
     
     protected function SetUpLinks() {
         $descriptors = $this->GetLinks();
         foreach ($descriptors as $link) {
-            
+            $this->SetupLink($link['link'],$link['target']);
         }
+    }
+    
+    protected function SetupLink($link,$target) {
+        $link = $this->GetCompletePath($link);
+        if (substr($target,0,2) !== '..') {
+            // absolute link
+            $target = $this->GetCompletePath($target);
+        }
+        exec("ln -s '".$target."' '".$link."'");
     }
     
     abstract function GetFiles();
--- tests/Unit/ScenarioWithFilesTest.php
+++ tests/Unit/ScenarioWithFilesTest.php
@@ -14,19 +14,24 @@ class ScenarioWithFilesTestScenario extends ScenarioBase{
         
         protected function GetFiles() {
             return [
-                
+                ['path'=>'/test.txt','content'=>'ABCD'],
+                ['path'=>'/test/test.txt','content'=>'AAAA'],
+                ['path'=>'/test/subdir/test.txt','content'=>'BBB'],                
             ];
         }
         
         protected function GetDirs() {
             return [
-                
+                '/test',
+                '/test/subdir',
+                '/test/subdir2',                
             ];
         }
         
         protected function GetLinks() {
             return [
-                
+                ['link'=>'link.txt','target'=>'test.txt'],
+                ['link'=>'/test/subdir/link.txt','target'=>'../../test.txt']
             ];
         }
         
@@ -37,9 +42,14 @@ class ScenarioWithFilesTest extends SunhillTestCase
    
     use CreatesApplication;
 
+    /**
+     * Depends on a functioning SetTarget and ClearTarget
+     * @return \Sunhill\Basic\Tests\Unit\ScenarioWithFilesTestScenario
+     */
     protected function SetupScenario() {
         $test = new ScenarioWithFilesTestScenario();
         $this->callProtectedMethod($test,'SetTarget',[$this->GetTempDir()]);
+        $this->callProtectedMethod($test,'ClearTarget',[]);
         return $test;
     }
     
@@ -56,10 +66,73 @@ class ScenarioWithFilesTest extends SunhillTestCase
     }
     
     public function testClearTarget() {
-        $test = $this->SetupScenario();
+        $test = new ScenarioWithFilesTestScenario();
+        $this->callProtectedMethod($test,'SetTarget',[$this->GetTempDir()]);
         exec('touch '.$this->GetTempDir().'/test.txt');
         $this->assertTrue(file_exists($this->GetTempDir().'/test.txt'));
         $this->callProtectedMethod($test,'ClearTarget',[]);
         $this->assertFalse(file_exists($this->GetTempDir().'/test.txt'));
     }
+    
+    public function testSetupDir() {
+        $test = $this->SetupScenario();
+        $this->callProtectedMethod($test,'SetupDir',['/test']);
+        $this->assertTrue(is_dir(storage_path('/temp/test')));
+    }
+    
+    /**
+     * This is a feature test
+     */
+    public function testSetupDirs() {
+        $test = $this->SetupScenario();
+        $this->callProtectedMethod($test,'SetupDirs',[]);
+        $this->assertTrue(is_dir(storage_path('/temp/test/subdir')));
+        $this->assertTrue(is_dir(storage_path('/temp/test/subdir2')));
+    }
+    
+    public function testSetupFile() {
+        $test = $this->SetupScenario();
+        $this->callProtectedMethod($test,'SetupFile',['/test.txt','ABCDEF']);
+        $this->assertEquals('ABCDEF',file_get_contents(storage_path('/temp/test.txt')));
+    }
+
+    /**
+     * This is a feature test
+     */
+    public function testSetupFiles() {
+        $test = $this->SetupScenario();
+        exec('mkdir -p '.storage_path().'/temp/test/subdir');
+        $this->callProtectedMethod($test,'SetupFiles',[]);
+        $this->assertEquals('ABCD',file_get_contents(storage_path('/temp/test.txt')));
+        $this->assertEquals('AAAA',file_get_contents(storage_path('/temp/test/test.txt')));
+        $this->assertEquals('BBB',file_get_contents(storage_path('/temp/test/subdir/test.txt')));
+    }
+    
+    public function testSetupLink_absolute() {
+        $test = $this->SetupScenario();
+        $this->callProtectedMethod($test,'SetupFile',['/test.txt','ABCDEF']);
+        
+        $this->callProtectedMethod($test,'SetupLink',['/link.txt','/test.txt']);
+        $this->assertEquals('ABCDEF',file_get_contents(storage_path('/temp/link.txt')));
+    }
+    
+    public function testSetupLink_relative() {
+        $test = $this->SetupScenario();
+        $this->callProtectedMethod($test,'SetupFile',['/test.txt','ABCDEF']);
+        exec('mkdir -p '.storage_path().'/temp/test/subdir');
+        
+        $this->callProtectedMethod($test,'SetupLink',['/test/subdir/link.txt','../../test.txt']);
+        $this->assertEquals('ABCDEF',file_get_contents(storage_path('/temp/test/subdir/link.txt')));
+    }
+    
+    public function testSetupLinks() {
+        $test = $this->SetupScenario();
+        $this->callProtectedMethod($test,'SetupFile',['/test.txt','ABCDEF']);
+        exec('mkdir -p '.storage_path().'/temp/test/subdir');
+        
+        $this->callProtectedMethod($test,'SetupLinks',[]);
+        $this->assertEquals('ABCDEF',file_get_contents(storage_path('/temp/link.txt')));
+        $this->assertEquals('ABCDEF',file_get_contents(storage_path('/temp/test/subdir/link.txt')));
+    }
+    
 }
