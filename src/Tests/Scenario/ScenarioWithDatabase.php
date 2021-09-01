b54cf248ee0c32928d77a2b5f84806e2562fe9ee
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-09-01 13:11:14 +0200

    Split into SetupDatabase() and SetupTables()

--- src/Tests/Scenario/ScenarioWithDatabase.php
+++ src/Tests/Scenario/ScenarioWithDatabase.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @file ScenarioWithDatabase.php
- * An extension to scenarios that handle handle databases
+ * An extension to scenarios that handle handle the creation of database tables
  * Lang en
- * Reviewstatus: 2021-08-05
+ * Reviewstatus: 2021-09-01
  * Localization: incomplete
  * Documentation: complete
  * Tests: 
@@ -15,21 +15,19 @@ namespace Sunhill\Basic\Tests\Scenario;
 use Illuminate\Support\Facades\DB;
 
 trait ScenarioWithDatabase {
-
-    protected $references = [];
     
     /**
      * Creates all the tables that are defined in the GetTableDescriptors function
      */
     protected function SetUpDatabase() {
         $this->references = [];
-        $descriptors = $this->GetTableDescriptors();
+        $descriptors = $this->GetDatabase();
         if (empty($descriptors)) {
             return; // Perhaps no new tables have to be created
         }
         if (!is_array($descriptors)) {
             // We expect an array as a function result, if not this is an error
-            throw \Exception("GetTableDescriptors doesn't return an array");
+            throw \Exception("GetDatabase doesn't return an array");
         }
         foreach ($descriptors as $table => $descriptor) {
             $this->setupTable($table,$descriptor);
@@ -57,74 +55,6 @@ trait ScenarioWithDatabase {
         }
         return $query_str.');';
     }
-    
-    protected function SetUpTables() {
-        $descriptors = $this->GetTableContents();
-        if (empty($descriptors)) {
-            return; // Perhaps no tables have to be filled
-        }
-        if (!is_array($descriptors)) {
-            // We expect an array as a function result, if not this is an error
-            throw \Exception("GetTableContents doesn't return an array");
-        }
-        foreach ($descriptors as $table => $descriptor) {
-            $this->fillTable($table,$descriptor);
-        }        
-    }
-    
-    protected function fillTable(string $tablename,array $descriptor) {
-        $fields = $descriptor[0];
-        $values = $descriptor[1];
-        foreach ($values as $reference => $realvalues) {
-            $this->insertSingleValue($tablename,$fields,$reference,$realvalues);
-        }
-    }
-    
-    protected function insertSingleValue($tablename,$fields,$reference,$values) {
-        $query_str = $this->getInsertQueryStr($tablename,$fields,$values);
-        DB::statement($query_str);
         
-        if (is_string($reference)) {
-            $id = DB::getPdo()->lastInsertId();
-            $this->references[$reference] = $id;
-        }        
-    }
-    
-    protected function getInsertQueryStr($tablename,$fields,$values) {
-        $result = "insert into $tablename (";
-        $first = true;
-        foreach ($fields as $field) {
-            $result .= ($first?'':',').$field;
-            $first = false;
-        }
-        $result .= ') values (';
-        $first = true;
-        foreach ($values as $value) {
-            $result .= $first?'':',';
-            if (is_null($value) || ($value === 'NULL') || ($value === '=>NULL')) {
-                $result .= 'NULL';
-            } else  if (is_string($value) && (substr($value,0,2) == '=>')) {
-                $result .= "'".$this->getReference($value)."'";
-            } else {
-                $result .= "'".$value."'";
-            }
-            $first = false;
-        }
-        return $result.')';
-    }
-    
-    protected function getReference($reference) {
-        $reference = substr($reference,2);
-        if (strpos($reference,'->')) {
-            $reference = substr($reference,0,strpos($reference,'->'));       
-        }
-        if (isset($this->references[$reference])) {
-            return $this->references[$reference];
-        } else {
-            throw new \Exception("Unknown reference '$reference'");
-        }
-    }
-    
-    abstract function GetTableDescriptors();
-    abstract function GetTableContents();        
+    abstract function GetDatabase();
 }
