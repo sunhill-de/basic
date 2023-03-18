24adbadad9b354b978092649c257b751abaea18c
Klaus Dimde <klaus@dimde.info>
2023-03-18 13:31:39 +0100

    Made sunhill:check look a little bit like phpunit

--- src/Console/Check.php
+++ src/Console/Check.php
@@ -9,20 +9,45 @@ class Check extends Command
 {
     protected $signature = 'sunhill:check {--repair}';
     
-    protected $description = 'Checks the consistency of the database and other structures';
+    protected $description = 'Checks the consistency of sunhill databases and structures';
     
     public function handle()
     {
         $this->info(__('Performing checks...',[]));
-        $result = Checks::Check($this->option('repair'));
-        foreach ($result as $single_result) {
-            if ($single_result->result == 'OK') {
-                $this->info($single_result->name.'... OK');
-            } else {
-                $this->error($single_result->name.'... FAILED:');
-                $this->error('  '.$single_result->error);
+        $repair = $this->option('repair');
+        $row = 0;
+        $output = $this->output;
+        $result = Checks::Check($repair, function($checker, $checks) use (&$output, &$row) {
+            switch ($checker->getLastResult()) {
+                case 'passed':
+                    $output->write(".",false);
+                    break;
+                case 'failed':
+                    $output->write("F",false);
+                    break;
+                case 'repaired':
+                    $output->write("R",false);
+                    break;
+                case 'unrepairable':
+                    $output->write("U",false);
+                    break;
             }
+/*            if (!($row++ % 63)) {
+                $output->writeln($checks->getTestsPerformed().' / '.$checks->getTotalTests()); 
+            } */
+        });
+        $params = [
+            'run'=>Checks::getTestsPerformed(),
+            'passed'=>Checks::getTestsPassed(),
+            'failed'=>Checks::getTestsFailed(),
+            'repaired'=>Checks::getTestsRepaired(),
+            'unrepairable'=>Checks::getTestsUnrepairable()
+        ];
+        $this->newLine();
+        if ($repair) {
+            $this->info(__('Checks finished (:run checks run: :passed passed, :failed failed, :repaired repaired, :unrepairable unrepairable)',$params));
+        } else {
+            $this->info(__('Checks finished (:run checks run: :passed passed, :failed failed, repair not set)',$params));            
         }
-        $this->info(__('Checks finished',[]));
     }
 }
